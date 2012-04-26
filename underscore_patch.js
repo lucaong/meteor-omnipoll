bd7c4f53a1dd0e55a8c4925570b9361412de18b4
Luca Ongaro <lukeongaro@gmail.com>
2012-04-26 15:36:10 +0200

    removed old files, gitignored .DS_Store

--- underscore_patch.js
+++
@@ -1,8 +0,0 @@
-_.pick = function(obj) {
-	var result = {},
-		slice = Array.prototype.slice;
-	_.each(_.flatten(slice.call(arguments, 1)), function(key) {
-		if (key in obj) result[key] = obj[key];
-	});
-	return result;
-};
